3593f856d888a361869771f89d1229acaccd829c
marivuolahti <78043421+marivuolahti@users.noreply.github.com>
2022-11-01 15:45:42 +0200

    Intial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+	{
+		path: '',
+		loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule),
+		...canActivate(redirectLoggedInToHome)
+	},
+	{
+		path: 'home',
+		loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+		...canActivate(redirectUnauthorizedToLogin)
+	},
+	{
+		path: '**',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'marisdemoapp',
+    appId: '1:981872704004:web:c2a5cf3ee6d2f9395cd351',
+    storageBucket: 'marisdemoapp.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCWJQW8f2p_vazZYOmqZNGzHqUyhxFgsmY',
+    authDomain: 'marisdemoapp.firebaseapp.com',
+    messagingSenderId: '981872704004',
+  },
   production: true
 };
